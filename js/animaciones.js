57f13001dbd5df7a101a404c0c4e89c3312036ae
Jhon Robert Vitonera <jhon.matamoros@air-fi.es>
2021-09-15 12:39:03 +0200

    mailchimp

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,64 +1,80 @@
-import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+let animados = document.querySelectorAll(".swiper-slide");
+let pantallas = document.querySelectorAll(".swiper-wrapper div")
+let verdaderoAnimados = document.querySelectorAll('.animado')
+const alturaViewport = window.innerHeight
 
+const arriba = 0
+let ultimaAlturaSlider = 0;
+let actualSlider = 0
 
+function getUltimaAlturaSlider() {
+    for ( var i = 0; i  < animados.length; i++) {
+        if (animados[i].classList.contains("swiper-slide-active")) {
+            console.log(animados[i].offsetTop)
+            return animados[i].offsetTop
 
-let animados = document.querySelectorAll(".animado");
-var porcentajeAlturaViewport = window.innerHeight / 100;
-
-const print =  (mensaje, variable, i) => {
-    console.log(`altura de ${mensaje}: ${variable} de ${i} `)
+        }
+    }
+    return 0
 }
-var scrollPos = 0;
 
 
+
+ultimaAlturaSlider = getUltimaAlturaSlider()
 function removeIfContains(obj, clase) {
     if (obj.classList.contains(clase)) {
         obj.classList.remove(clase)
     }
 }
-var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    slidesPerView: 1,
-    mousewheel: true,
-    
-  });
+function quitarOtras(object, i, string1, string2) {
+    for (y=0; y < object.length; y++) {
+        if (y != i) {
+            removeIfContains(object[y], string1)
+            removeIfContains(object[y], string2)
+            
+        }
+    }
+    object[i].classList.add(string1)
+}
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 function mostrarScroll() {
-    
-    let scrollTop = document.documentElement.scrollTop;
+    actualSlider = getUltimaAlturaSlider()
     for (var i=0; i < animados.length; i++) {
         let animado = animados[i]
         let alturaAnimado = animado.offsetTop;
-        console.log("ada")
-        print('ada', scrollTop, i)
-        if ((document.body.getBoundingClientRect()).top > scrollPos) {
-            if(alturaAnimado - 35 * porcentajeAlturaViewport < scrollTop && 
-                scrollTop < alturaAnimado + -10 * porcentajeAlturaViewport) {
-                animado.style.opacity = 1;
-                animado.classList.add('desdeArriba')
-            }
-    
-            else {
-                animado.style.opacity = 0;
-                removeIfContains(animado, 'desdeArriba')
-                removeIfContains(animado, 'desdeAbajo')    
-                
-            }
+        let diferencia =  alturaAnimado - ultimaAlturaSlider
+        console.log(`diferencia ${diferencia}; alturaSlider: ${ultimaAlturaSlider}; i: ${i}` )
+        if (diferencia > 0 && actualSlider == alturaViewport * i) {
+            quitarOtras(verdaderoAnimados, i , 'desdeArriba', 'desdeAbajo')
+            ultimaAlturaSlider = getUltimaAlturaSlider()
+            return
         }
-            
-        else
-            if(alturaAnimado - 35 * porcentajeAlturaViewport < scrollTop && 
-                scrollTop < alturaAnimado + 20 * porcentajeAlturaViewport) {
-                animado.style.opacity = 1;
-                animado.classList.add('desdeAbajo')
-            }
+        else if (diferencia < 0 && actualSlider == alturaViewport * i) {
+            quitarOtras(verdaderoAnimados, i, 'desdeAbajo', 'desdeArriba')
+            ultimaAlturaSlider = getUltimaAlturaSlider()
+            return
+        }
+        
 
-            else {
-                animado.style.opacity = 0;
-                removeIfContains(animado, 'desdeArriba')
-                removeIfContains(animado, 'desdeAbajo')                
-            }
     }
+
+    ultimaAlturaSlider = getUltimaAlturaSlider()
 }
 
-window.addEventListener('scroll', mostrarScroll)
+document.querySelector('.swiper-container').addEventListener('wheel', debounce(mostrarScroll, 100), false)
+document.querySelector('.swiper-container').addEventListener('touchmove', mostrarScroll)
